5c98a9b632dea24138f32ad42129c18d93725b82
Mr. Lee <>
2019-03-10 17:43:42 +0800

    第一個 DOM XML 展示

--- src/main/java/com/bigbigmall/xiamen/controller/WelcomeController.java
+++ src/main/java/com/bigbigmall/xiamen/controller/WelcomeController.java
@@ -1,7 +1,19 @@
 package com.bigbigmall.xiamen.controller;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * @author P-C Lin (a.k.a 高科技黑手)
@@ -11,6 +23,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
 
 	@RequestMapping("/")
-	void index() {
+	@ResponseBody
+	void index(HttpServletResponse response) throws ParserConfigurationException, TransformerConfigurationException, IOException, TransformerException {
+		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		Element documentElement = document.createElement("document");
+		document.appendChild(documentElement);
+
+		TransformerFactory.newInstance().newTransformer().transform(new DOMSource(document), new StreamResult(response.getOutputStream()));
 	}
 }
