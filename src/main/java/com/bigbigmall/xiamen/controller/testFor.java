89f0722f051449f714f6770b58e7f6e73444a3f0
yjc_glb <734311568@qq.com_glb>
2019-03-22 23:51:11 +0800

    调整好了质数为红的

--- src/main/java/com/bigbigmall/xiamen/controller/testFor.java
+++ src/main/java/com/bigbigmall/xiamen/controller/testFor.java
@@ -12,9 +12,9 @@ package com.bigbigmall.xiamen.controller;
 public class testFor {
 
         public static void main(String[] args) {
-                for (int i =1; i <= 18; i++) {
-                        for (int j = 1; j <= 5; j++) {
-                                System.out.print("&&&&&&&&&\t");
+                for (int i =1; i <=9; i++) {
+                        for (int j = 1; j <i; j++) {
+                                System.out.print(i*j+" ");
                         }
                          System.out.println(" ");
 
